a96a82e4bcc282e9d1e2e9baf4b78b5d129bbd19
Mayara Miyazaki <mayaramiyazakipng.senai@gmail.com>
2025-06-11 16:04:11 -0300

    Pdf sete copiado

---
+++ src/main/java/br/com/miyazaki/OSApiApplication/domain/service/OrdemServicoService.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.miyazaki.OSApiApplication.domain.service;
+
+import br.com.miyazaki.OSApiApplication.domain.model.OrdemServico;
+import br.com.miyazaki.OSApiApplication.domain.model.StatusOrdemServico;
+import br.com.miyazaki.OSApiApplication.domain.repository.OrdemServicoRepository;
+import java.time.LocalDateTime;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author ppjatb
+ */
+@Service    
+public class OrdemServicoService {
+    
+    @Autowired
+    private OrdemServicoRepository ordemServicoRepository;
+    
+    public OrdemServico criar(OrdemServico ordemServico){
+        ordemServico.setStatus(StatusOrdemServico.ABERTA);
+        ordemServico.setDataAbertura(LocalDateTime.now());
+        
+        return ordemServicoRepository.save(ordemServico);
+    }
+}
